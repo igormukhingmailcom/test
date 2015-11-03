7f098f8a83859a8e64f040fd1c0a016aa543a895
Igor Mukhin <igor.mukhin@gmail.com>
2015-11-03 15:41:05 +0200

    Added: Env output

--- web/index.php
+++ web/index.php
@@ -6,4 +6,7 @@ $calculator = new TestApp\Calculator();
 $result = $calculator->calculate(2,3);
 
 echo sprintf("Result is: %s<br/>", $result);
-echo sprintf("Time: %s", date('Y-m-d H:i:s'));
+echo sprintf("Time: %s<br/>", date('Y-m-d H:i:s'));
+echo sprintf("Env variable TEST: %s</br>", getenv('TEST'));
+
+phpinfo(INFO_ENVIRONMENT);
